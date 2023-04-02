196b5ca0e7717dd05d58094b325585245a27045e
Sidharth Shukla <swayam1044@gmail.com>
2023-04-02 11:09:01 +0530

    57.11

---
+++ client/src/canvas/CameraRig.jsx
@@ -0,0 +1,19 @@
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+
+const CameraRig = ({children}) => {
+
+    
+  return 
+   <group>
+    {childern}
+   </group>
+ 
+}
+
+export default CameraRig
